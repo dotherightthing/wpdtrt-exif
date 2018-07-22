23f41bdcce534a3dcbfffb31a2ab11631356e891
Dan Smith <dev@dotherightthing.co.nz>
2018-07-23 08:58:22 +1200

    Bump version

--- src/class-wpdtrt-exif-plugin.php
+++ src/class-wpdtrt-exif-plugin.php
@@ -12,7 +12,7 @@
  *
  * @since   1.0.0
  */
-class WPDTRT_Exif_Plugin extends DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_4_39\Plugin {
+class WPDTRT_Exif_Plugin extends DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_5_0\Plugin {
 
 	/**
 	 * Supplement plugin initialisation.
