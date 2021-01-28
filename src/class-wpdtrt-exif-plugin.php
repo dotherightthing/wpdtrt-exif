e5364ba1d4c33a4d5490a55fd2e551044a7873ba
Dan Smith <dev@dotherightthing.co.nz>
2021-01-28 23:19:10 +1300

    Update wpdtrt-plugin-boilerplate from 1.7.12 to 1.7.13

--- src/class-wpdtrt-exif-plugin.php
+++ src/class-wpdtrt-exif-plugin.php
@@ -12,7 +12,7 @@
  *
  * @since   1.0.0
  */
-class WPDTRT_Exif_Plugin extends DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_7_12\Plugin {
+class WPDTRT_Exif_Plugin extends DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_7_13\Plugin {
 
 	/**
 	 * Supplement plugin initialisation.
